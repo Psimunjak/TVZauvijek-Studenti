a15403a47f905edf043a82f0ceb0bd1715317d6b
MatejAndelkoBoskovic <106883956+MatejAndelkoBoskovic@users.noreply.github.com>
2022-06-09 17:40:27 +0200

    Add files via upload

---
+++ generatorPitanja.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+#include<stdio.h>
+#include<dos.h>
+
+using namespace std;
+
+void StartTimer(int time)
+{
+    while (time > 0)
+    {
+        Sleep(1000);
+        time--;
+    }
+}
+void vrstapitanja(char op) {
+    int var1, var2, odgovor;
+    bool tocno = false;
+    var1 = rand() % 50 + 1;
+    var2 = rand() % 50 + 1;
+    std::cout << "Koliko je: " << var1 << " " << op << " " << var2 << "? ";
+    StartTimer(5);
+    std::cin >> odgovor;
+    switch (op) {
+    case '+':
+        if (odgovor == var1 + var2) tocno = true;
+        break;
+
+    case '-':
+        if (odgovor == var1 - var2) tocno = true;
+        break;
+
+    case '*':
+        if (odgovor == var1 * var2) tocno = true;
+        break;
+
+    case '/':
+        if (odgovor == var1 / var2) tocno = true;
+        break;
+    }
+    if (tocno == true)
+    {
+        cout << "Bravo!" << endl;
+    }
+    else {
+        cout << "Netocno!!!" << endl;
+    }
+}
+
+int main() {
+
+    int var1, var2, odgovor;
+    vrstapitanja('+');
+    vrstapitanja('+');
+    vrstapitanja('-');
+    vrstapitanja('*');
+    vrstapitanja('/');
+    return 0;
+}
---
+++ postepenoPovecavanjeTezine.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    int b=0;
+    cout << "Podesite interval između tezina: " << endl;
+    cin >> n;
+    int milli_seconds = n * 1000;
+    while (b < 9)
+    {
+        
+        b = b + 1;
+        cout << "trenutna težina: " << b << endl;
+        Sleep(milli_seconds);
+    }
+
+
+    return 0;
+}
